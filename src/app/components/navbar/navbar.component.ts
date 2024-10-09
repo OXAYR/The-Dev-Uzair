87e4975e50441ff71ed658073a3b2be910b9a5f0
Oxayr <uzairmaqsood451@gmail.com>
2024-10-09 14:02:02 +0500

    added the navRoutes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   navRoutes: Array<{ name: string; path: string }> = [
-    { name: 'Home', path: '/home' },
-    { name: 'Work', path: '/work' },
-    { name: 'About Me', path: '/about-me' },
-    { name: 'Contacts', path: '/contacts' },
+    { name: 'home', path: '/home' },
+    { name: 'work', path: '/work' },
+    { name: 'about-me', path: '/about-me' },
+    { name: 'contacts', path: '/contacts' },
   ];
 }
