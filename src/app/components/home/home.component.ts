132c0fafecf852e77e281b3760d475d46979c743
Oxayr <uzairmaqsood451@gmail.com>
2024-10-10 12:10:14 +0500

    added the project card in the home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
+import { ProjectCardComponent } from '../project-card/project-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, ProjectCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
