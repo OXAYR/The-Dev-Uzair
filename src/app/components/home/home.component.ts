3d416dbceb60af6fcfb66aec9d29501f94409178
Oxayr <uzairmaqsood451@gmail.com>
2024-10-10 16:35:45 +0500

    added the skilss area

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { ProjectCardComponent } from '../project-card/project-card.component';
 import { SkillsCardComponent } from '../skills-card/skills-card.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, ProjectCardComponent, SkillsCardComponent],
+  imports: [
+    HeroComponent,
+    ProjectCardComponent,
+    SkillsCardComponent,
+    AboutMeComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
