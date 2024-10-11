e24410e728b498ada5078e8ea366d61accfb98ac
Oxayr <uzairmaqsood451@gmail.com>
2024-10-11 17:19:48 +0500

    added about us

--- src/app/components/about-me-page/about-me-page.component.ts
+++ src/app/components/about-me-page/about-me-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { AboutMeComponent } from '../about-me/about-me.component';
+import { SkillsCardComponent } from '../skills-card/skills-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-me-page',
   standalone: true,
-  imports: [AboutMeComponent],
+  imports: [AboutMeComponent, SkillsCardComponent, CommonModule],
   templateUrl: './about-me-page.component.html',
   styleUrl: './about-me-page.component.css',
 })
-export class AboutMePageComponent {}
+export class AboutMePageComponent {
+  funFacts = [
+    'I like winter more than summer',
+    'I often bike with my friends',
+    'I like pizza and pasta',
+  ];
+}
