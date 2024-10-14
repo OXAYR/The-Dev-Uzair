f51a4e5a901cf9d109cb8405c3ffd6a988ad2e59
Oxayr <uzairmaqsood451@gmail.com>
2024-10-14 18:50:51 +0500

    remove the typo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: false,
-  appVersion: '1.0.0',
   GITHUB_TOKEN: '',
 };
--- src/environments/environments.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  appVersion: '1.0.0',
-  GITHUB_TOKEN: '',
-};
