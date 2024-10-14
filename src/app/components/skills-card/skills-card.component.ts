a3585335a324a27562679925e7e8a7a828431db5
Oxayr <uzairmaqsood451@gmail.com>
2024-10-14 16:30:56 +0500

    update the repo thumbnail

--- src/app/components/skills-card/skills-card.component.ts
+++ src/app/components/skills-card/skills-card.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills-card.component.html',
-  styleUrl: './skills-card.component.css'
+  styleUrl: './skills-card.component.css',
 })
 export class SkillsCardComponent {
-
+  skillset = [
+    { heading: 'Language', text: 'typescript Javascript Python ' },
+    {
+      heading: 'Database',
+      text: 'MY-SQL Mongo Firebase',
+    },
+    {
+      heading: 'Tools',
+      text: 'VSCode Git Figma',
+    },
+    {
+      heading: 'Other',
+      text: 'HTML CSS REST',
+    },
+    {
+      heading: 'Frameworks',
+      text: 'Angular React Vue Node Express React Native',
+    },
+  ];
 }
